7e787da53d194e6986fa86719f3a4f4538e7d60b
Jay Palat <palatv@upmc.edu>
2014-01-28 16:58:44 -0500

    Added View Controller.

--- Fruit/ViewController.m
+++ Fruit/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "Fruit.h"
 
 @interface ViewController ()
+@property (nonatomic, strong) NSMutableArray *cart;
 
 @end
 
@@ -18,6 +20,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.cart = [[NSMutableArray alloc] init];
+    NSString *itemName;
+    NSString *fruitName = @"Bananas";
+    
+    for (int i = 0; i<50; i++) {
+        itemName = [[NSString alloc] initWithFormat:@"%@ %d", fruitName, i];
+        Fruit *tempFruit = [[Fruit alloc] initWithName:itemName andShape:@"Curved" andColor:@"Yellow"];
+        [self.cart addObject:tempFruit];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +36,23 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.cart count];
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    NSString *identifier = @"fruitCell";
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+    
+    cell.textLabel.text = [[self.cart objectAtIndex:[indexPath row]] name];
+    return cell;
+    
+}
+
+
+
 
 @end

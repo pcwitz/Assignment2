c521b3c59425886075b2c5823757af01fcceac71
Build User <builduser@ci-osx-executor.local>
2014-01-28 10:48:37 -0500

    Initial Commit

---
+++ Fruit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fruit
+//
+//  Created by Build User on 1/28/14.
+//  Copyright (c) 2014 Pitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
